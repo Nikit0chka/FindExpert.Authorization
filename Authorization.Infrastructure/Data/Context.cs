22a5026632a28fe756ff3c74bebae4fb906c59aa
Nikit0chka <vorontsov.nikita.job@gmail.com>
2025-02-24 22:33:05 +0400

    Added fluent api database configuration. Changed a little domain models
    Signed-off-by: Nikit0chka <vorontsov.nikita.job@gmail.com>

--- Authorization.Infrastructure/Data/Context.cs
+++ Authorization.Infrastructure/Data/Context.cs
@@ -1,5 +1,6 @@
 ﻿using Authorization.Domain.AggregatesModel.AuthorizedSessionAggregate;
 using Authorization.Domain.AggregatesModel.UserAggregate;
+using Authorization.Infrastructure.Data.Configs;
 using Microsoft.EntityFrameworkCore;
 
 namespace Authorization.Infrastructure.Data;
@@ -14,4 +15,12 @@ internal sealed class Context:DbContext
 
     public DbSet<User> Users { get; init; }
     public DbSet<AuthorizedSession> AuthorizedSessions { get; init; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new AuthorizedSessionConfiguration());
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
